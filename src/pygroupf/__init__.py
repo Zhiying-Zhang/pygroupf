5638dd33e738ee4e9dac98e0a09c54c7673613ca
Zhiying-Zhang <zhiying_zhang@outlook.com>
2025-03-30 11:23:19 +0200

    edit license and pyproject

--- src/pygroupf/__init__.py
+++ src/pygroupf/__init__.py
@@ -1,5 +1,1 @@
 #Let python know this dialoge is a package
-from pygroupf.data_processing import*
-from pygroupf.analysis import DataAnalyzer
-from pygroupf.visualization import DataVisualizer
-from pygroupf.modeling import* 
